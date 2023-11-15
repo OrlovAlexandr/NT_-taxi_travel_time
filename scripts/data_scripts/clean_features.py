ee90bc4fcc0cfffa442e383d86cdabd5449fc865
alex <eaglophone@gmail.com>
2023-11-15 17:06:16 +0500

    implement dvc pipeline

---
+++ scripts/data_scripts/clean_features.py
@@ -0,0 +1,33 @@
+import os
+import sys
+
+import pandas as pd
+
+if len(sys.argv) != 2:
+    sys.stderr.write("Arguments error. Usage:\n")
+    sys.stderr.write("\tpython3 clean_features.py data-file\n")
+    sys.exit(1)
+
+data_path = sys.argv[1]
+print('data_path:', data_path)
+
+f_output = os.path.join("data", "stage2", "dataset_cleaned.csv")
+os.makedirs(os.path.join("data", "stage2"), exist_ok=True)
+
+# Read dataset
+data = pd.read_csv(data_path)
+
+# Starting a trip, we can never know exactly when it will end, as we are
+# trying to predict the end time of the trip.
+drop_columns = ['id', 'dropoff_datetime']
+data = data.drop(drop_columns, axis=1)
+print('Columns left after cleaning:', data.shape[1])
+
+# Earlier, we extracted all the necessary information from the date of
+# the trip
+drop_columns = ['pickup_datetime', 'pickup_date']
+data = data.drop(drop_columns, axis=1)
+print('Shape of data:  {}'.format(data.shape))
+
+# Saving dataframe to file
+data.to_csv(f_output, index=False)
--- scripts/data_scripts/get_features.py
+++ scripts/data_scripts/get_features.py
@@ -1,37 +1,53 @@
+import os
+import sys
+
 import pandas as pd
 
-from scripts.data_scripts import add_features as af
+import add_features as af
 
-taxi_data = pd.read_csv("datasets/train.csv")
+if len(sys.argv) != 5:
+    sys.stderr.write("Arguments error. Usage:\n")
+    sys.stderr.write("\tpython3 get_features.py data-file\n")
+    sys.exit(1)
 
+train_path = sys.argv[1] #osrm
+holiday_path = sys.argv[2] #train
+osrm_path = sys.argv[3] #holiday
+weather_path = sys.argv[4] #weather
+print('train_path', train_path)
+print('holiday_path', holiday_path)
+print('osrm_path', osrm_path)
+print('weather_path', weather_path)
+
+
+f_output = os.path.join("data", "stage1", "taxi_dataset.csv")
+os.makedirs(os.path.join("data", "stage1"), exist_ok=True)
+
+taxi_data = pd.read_csv(train_path)
 print('Train data shape: {}'.format(taxi_data.shape))
 
 # Convert to datetime
 taxi_data['pickup_datetime'] = pd.to_datetime(taxi_data['pickup_datetime'],
                                               format='%Y-%m-%d %H:%M:%S')
-
-# Missing data
 print(f'\nMissing data in train dataframe: {taxi_data.isna().sum().sum()}')
 
 # Adding datetime features
 taxi_data = af.add_datetime_features(taxi_data)
 
 # Adding holiday features
-holiday_data = pd.read_csv('datasets/holiday_data.csv', sep=';')
+holiday_data = pd.read_csv(holiday_path, sep=';')
 holiday_data['date'] = pd.to_datetime(holiday_data['date'],
                                       format='%Y-%m-%d').dt.date
 
 taxi_data = af.add_holiday_features(taxi_data, holiday_data)
 
 # Adding OSRM features
-osrm_data = pd.read_csv('datasets/osrm_data_train.zip',
+osrm_data = pd.read_csv(osrm_path,
                         usecols=['id', 'total_distance', 'total_travel_time',
                                  'number_of_steps']
                         )
 
 taxi_data = af.add_osrm_features(taxi_data, osrm_data)
-
-# Missing data
 print(f"Missing data after adding OSRM: {taxi_data['total_distance'].isna().sum()}")
 
 # Adding geographical features
@@ -41,7 +57,7 @@ af.add_geographical_features(taxi_data)
 af.add_cluster_features(taxi_data)
 
 # Adding weather features
-weather_data = pd.read_csv('datasets/weather_data.zip',
+weather_data = pd.read_csv(weather_path,
                            usecols=['date', 'hour',
                                     'temperature', 'visibility',
                                     'wind speed', 'precip', 'events']
@@ -53,14 +69,10 @@ weather_data['pickup_hour'] = weather_data['hour'].astype('int64')
 weather_data = weather_data.drop(columns=['date', 'hour'])
 
 taxi_data = af.add_weather_features(taxi_data, weather_data)
-
-# Missing data
 print(f'\nMissing data after adding weather features: {taxi_data.isna().sum().sum()}')
 
 # Filling missing data in weather features
 af.fill_null_weather_data(taxi_data)
-
-# Missing data
 print(f'\nMissing data after filling null: {taxi_data.isna().sum().sum()}')
 
 # Remove extremely long trips
@@ -71,7 +83,4 @@ avg_speed = taxi_data['total_distance'] / taxi_data['trip_duration'] * 3.6
 taxi_data = taxi_data[avg_speed <= 300]
 
 # Saving dataframe to file
-taxi_data.to_csv('artifacts/taxi_data_df.csv', index=False)
-
-print('-' * 79 + '\n')
-print('Data creation is finished!')
+taxi_data.to_csv(f_output, index=False)
